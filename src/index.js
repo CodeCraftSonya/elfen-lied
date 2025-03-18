b28354f06762290b37f3c7f2a278d4d0bdd4ed67
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-03-18 15:02:53 +0300

    init: webpack config

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss';
+import tesla from './assets/tesla.jpg';
+
+
+const mainImage = document.getElementById("mainImage")
+mainImage.src = tesla;
+const hello = require('./js/hello.js');
