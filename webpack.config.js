da8cdf300fd88132cd830f866f1fd20f3594eebe
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-03-20 13:42:25 +0300

    feat: add icons, add html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,52 @@
-const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "production",
-    entry: {
-        filename: path.resolve(__dirname,"src/index.js"),
+  mode: 'production',
+  entry: {
+    filename: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    assetModuleFilename: '[name][ext]',
+    clean: true,
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-    output: {
-        path: path.resolve(__dirname,"dist"),
-        filename: "[name][contenthash].js",
-        assetModuleFilename: "[name][ext]",
-        clean: true
-    },
-    performance: {
-        hints: false,
-        maxAssetSize: 512000,
-        maxEntrypointSize: 512000
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'imgs/[name][ext]',
         },
-        compress: true,
-        port: 9000,
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource"
-            }
-        ]
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            filename: "index.html",
-            template: "src/index.html"
-        })
-    ]
-}
+      },
+    ],
+  },
+};
